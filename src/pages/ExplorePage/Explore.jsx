40ec2266f725eca4c4adaaa66af36bfbab9e0bac
sanunavwoo <sanunavodey@gmail.com>
2023-07-19 23:51:34 +0530

    Fix-Responsiveness for Login/Signup

--- src/pages/ExplorePage/Explore.jsx
+++ src/pages/ExplorePage/Explore.jsx
@@ -41,7 +41,9 @@ export function Explore(){
                     <div>
                         {isCreatePostFlag && <AddPost setIsCreatePostFlag={setIsCreatePostFlag} /> }
                     </div>
-                  
+                    <div>
+                        <h1>Explore</h1>
+                    </div>
                     {postsLoading?
                         <img className="loading-img" src="https://assets.materialup.com/uploads/a7e6009b-6d69-4569-b1ee-0e01b234f2a1/preview.gif" />
                     :
